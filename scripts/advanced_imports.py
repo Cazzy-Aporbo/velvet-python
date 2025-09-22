d13383fc2b26c6dc7cf1fad6a2fa20dc9a193e1e
Cazandra Aporbo <83199148+Cazzy-Aporbo@users.noreply.github.com>
2025-09-22 14:17:25 -0700

    Create advanced_imports.py

---
+++ scripts/advanced_imports.py
@@ -0,0 +1,228 @@
+"""
+Advanced Python Import Reference
+Author: Cazzy
+Purpose: Collection of highly advanced, niche, and powerful Python libraries for
+data science, mathematics, visualization, machine learning, and scientific computing.
+This file includes libraries that most developers may not know about.
+"""
+
+# ------------------------
+# Advanced Numerical Computation
+# ------------------------
+import numba
+import cupy
+import bottleneck
+import pyculib
+import sparse
+import tensorly
+import optuna
+import cvxpy
+import cvxopt
+import pymc3
+import pymc4
+import pyro
+import jax
+import jax.numpy as jnp
+import theano
+import aesara
+import mpi4py
+import symengine
+
+# ------------------------
+# Advanced Data Science and ML
+# ------------------------
+import lightgbm
+import xgboost
+import catboost
+import h2o
+import dlib
+import fasttext
+import vowpalwabbit
+import autograd
+import shap
+import lime
+import eli5
+import interpret
+import albumentations
+import torchmetrics
+import timm
+import detectron2
+import segmentation_models_pytorch
+import skorch
+
+# ------------------------
+# Advanced Visualization
+# ------------------------
+import plotly.figure_factory as ff
+import plotly.io as pio
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pyvista
+import vedo
+import mayavi.mlab as mlab
+import holoviews as hv
+import panel as pn
+import bokeh.layouts
+import bokeh.models
+import networkx as nx
+import graph_tool.all as gt
+import matplotlib.tri as mtri
+import geoplot
+import contextily
+import folium.plugins
+import cartopy.feature as cfeature
+import datashader as ds
+
+# ------------------------
+# Advanced Geospatial
+# ------------------------
+import rasterstats
+import geopandas
+import fiona
+import pyproj
+import shapely
+import rioxarray
+import xarray
+import regionmask
+import salem
+import pyresample
+import skgstat
+import pysal
+import h3
+import osmnx as ox
+import movingpandas
+
+# ------------------------
+# Advanced Statistics & Mathematics
+# ------------------------
+import statsmodels.api as sm
+import pingouin
+import linearmodels
+import sympy
+import mpmath
+import sage.all as sage
+import cvxpy
+import cvxopt
+import pyDOE
+import pyDOE2
+import copulas
+import lifelines
+import scikit-posthocs
+import bottleneck
+import fastcluster
+import networkx.algorithms.community as nx_comm
+
+# ------------------------
+# Time Series & Forecasting
+# ------------------------
+import prophet
+import tbats
+import sktime
+import darts
+import neuralprophet
+import statsforecast
+import arch
+import pmdarima
+import pyflux
+import tslearn
+import kats
+
+# ------------------------
+# Image, Video & Audio Processing
+# ------------------------
+import skimage
+import skimage.io as skio
+import scikit-image
+import imageio
+import SimpleITK as sitk
+import nibabel as nib
+import pydicom
+import librosa
+import pysoundfile as psf
+import audioread
+import pywavelets
+import moviepy.editor as mp
+import opencv-contrib-python
+
+# ------------------------
+# Advanced Web, Networking & APIs
+# ------------------------
+import selenium
+import playwright.sync_api as pw
+import requests_html
+import pyppeteer
+import aiohttp
+import websockets
+import socketio
+import websocket
+import scrapy
+import twython
+import tweepy
+import googlemaps
+
+# ------------------------
+# Advanced Optimization & Computational Geometry
+# ------------------------
+import scipy.optimize as opt
+import scikit-geometry as skg
+import trimesh
+import pyvista
+import vedo
+import networkx.algorithms.approximation as nx_approx
+import ortools
+import pymetis
+import pyomo
+import gekko
+
+# ------------------------
+# Experimental / Cutting-edge Tools
+# ------------------------
+import ray
+import modin.pandas as mpd
+import polars as pl
+import datatable as dt
+import vaex
+import turicreate as tc
+import bambi
+import aesara
+import pytorch_lightning as pl
+import fastai
+import lightning.pytorch
+import optuna
+import nevergrad
+import functorch
+import diffusers
+import accelerate
+import transformers
+
+# ------------------------
+# Advanced File Formats & Data Pipelines
+# ------------------------
+import h5py
+import netCDF4
+import zarr
+import pyarrow
+import feather
+import parquet
+import tables
+import xlwings
+import openpyxl
+import pdfplumber
+import PyPDF2
+import python-docx
+import odfpy
+
+# ------------------------
+# Parallelism and High Performance
+# ------------------------
+import dask
+import dask.array as da
+import dask.dataframe as dd
+import concurrent.futures
+import joblib
+import mpi4py
+import ray
+import parsl
+
+# End of Advanced Python Import Reference
